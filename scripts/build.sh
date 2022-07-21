47a46366527b532bc8400d7051f24b1a93e81894
Nartallax <nartallax@gmail.com>
2022-07-21 14:33:46 +0400

    1.0.1: added shebang to executable

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,8 @@ cd ..
 
 rm -rf target
 ./node_modules/.bin/imploder --tsconfig tsconfig.json
+
+echo "#!/usr/bin/env node" > ./target/result.js
+cat ./target/package_syncer.js >> ./target/result.js
+mv ./target/result.js ./target/package_syncer.js
+chmod +x ./target/package_syncer.js
